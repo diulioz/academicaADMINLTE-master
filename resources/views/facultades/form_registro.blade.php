9afb33b58620fedfbe6122157e055ba555f55d3b
DAVID JULIAN CGZ <diulioz.u@gmail.com>
2023-06-05 21:06:01 -0500

    Update form_registro.blade.php

--- resources/views/facultades/form_registro.blade.php
+++ resources/views/facultades/form_registro.blade.php
@@ -10,13 +10,13 @@
     <form>
     <div class="mb-3">
         <label for="codigo" class="form-label">Código</label>
-        <input type="text" class="form-control" id="codFacultad" placeholder="id">
+        <input type="number" class="form-control" id="codFacultad" placeholder="Código Facultad" require min='1'>
     </div>
     <div class="mb-3">
         <label for="nombre" class="form-label">Nombre de Facultad</label>
-        <input type="text" class="form-control" id="nomFacultad" placeholder="nombre">
+        <input type="text" class="form-control" id="nomFacultad" placeholder="Nombre Facultad" require>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-success">Registar</button>
 </form>
 @stop
 
