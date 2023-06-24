370fb9b62436f0abab6516cc70f48315268a8fd4
David Criollo <diulioz.u@gmail.com>
2023-06-24 15:43:48 -0500

    rutasestudiantesprofesores

--- routes/web.php
+++ routes/web.php
@@ -27,16 +27,24 @@ Route::get('/', function () {
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/facultades/listado', [Facultades::class, 'index'])->name('listadoFac');
 Route::get('/facultades/registrar', [Facultades::class, 'form_registro']);
-Route::get('/facultades/eliminar/{id}', [Facultades::class, 'eliminar'])->name('eliminaFac');
 Route::post('/facultades/registrar', [Facultades::class, 'registrar']);
+Route::get('/facultades/eliminar/{id}', [Facultades::class, 'eliminar'])->name('eliminaFac');
 
 Route::get('/programas/listado', [Programas::class, 'index'])->name('listadoProg');
 Route::get('/programas/registrar', [Programas::class, 'form_registro']);
-Route::get('/programas/eliminar/{id}', [Programas::class, 'eliminar'])->name('eliminaProg');
 Route::post('/programas/registrar', [Programas::class, 'registrar']);
+Route::get('/programas/eliminar/{id}', [Programas::class, 'eliminar'])->name('eliminaProg');
 
 Route::get('/profesores/listado', [Profesores::class, 'index'])->name('listadoProf');
+Route::get('/profesores/registrar', [Programas::class, 'form_registro']);
+Route::post('/profesores/registrar', [Programas::class, 'registrar']);
+Route::get('/profesores/eliminar/{id}', [Programas::class, 'eliminar'])->name('eliminaProf');
+
 Route::get('/estudiantes/listado', [Estudiantes::class, 'index'])->name('listadoEst');
+Route::get('/estudiantes/registrar', [Estudiantes::class, 'form_registro']);
+Route::post('/estudiantes/registrar', [Estudiantes::class, 'registrar']);
+Route::get('/estudiantes/eliminar/{id}', [Estudiantes::class, 'eliminar'])->name('eliminaEst');
+
 Route::get('/regnotas/listado', [Calificaciones::class, 'index']); //Debe ser igual al de adminlte.php
 
 
